afbcd1d47bcac224860abc3cc48a47e778620169
Steven Nguyen <80537958+daxile6@users.noreply.github.com>
2023-03-04 19:54:31 -0800

    Initial Push

---
+++ app/App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  VersionCheck,
+} from 'react-native';
+
+import {Quiz} from './screens';
+
+/*returning the quiz layout and functionality*/
+const App = () => {
+  
+  return (
+    <Quiz />
+  );
+}
+
+
+
+export default App;
---
+++ app/data/QuizData.js
@@ -0,0 +1,28 @@
+/* The questions and answer data to be used for the learning assement*/
+export default data = [
+    {
+        question: "What’s the biggest planet in our solar system?",
+        options: ["Jupiter","Saturn","Neptune","Mercury"],
+        correct_option: "Jupiter"
+    },
+    {
+        question: "How does a lizard regulate body heat?",
+        options: ["Chilli Sauce","Fire","Sunlight"],
+        correct_option: "Sunlight"
+    },
+    {
+        question: "What are the main fossils that customers go to the Natural History Museum for?",
+        options: ["Alligator","Crocodile","Dinosaurs","Snakes"],
+        correct_option: "Dinosaurs"
+    },
+    {
+        question: "What is the largest animal on Earth?",
+        options: ["The African elephant","The blue whale","The sperm whale","The giant squid"],
+        correct_option: "The blue whale"
+    },
+    {
+        question: "How many teeth does a T-Rex have?",
+        options: ["50","65","60","100"],
+        correct_option: "60"
+    }
+]
