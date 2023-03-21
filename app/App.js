f3c7fa4aa5079c38c9f7ab2f0445bf050305f6ad
Krimika Keemtee <krimikakeemtee@gmail.com>
2023-03-21 00:48:53 -0700

    Updated Controller.js
    - disabled prettier cause it was being a pain
    - Added dependencies

--- app/App.js
+++ app/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -13,10 +14,13 @@ import {
 } from 'react-native';
 
 import {Quiz} from './screens';
-import {DestinationQueue} from './screens'
+import {DestinationQueue} from './screens';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native'; //contains navigator and screen
 import { Screen } from 'react-native-screens';
+
+//krimika's controller thing:
+import {Controller} from './screens';
 
 const Stack = createNativeStackNavigator()
 /*returning the quiz layout and functionality*/
@@ -34,8 +38,11 @@ const App = () => {
         <Stack.Screen
           name = "QuizApp"
           component = {Quiz}
-          />
-
+        />
+        
+        <Stack.Screen 
+          name = "Controller"
+          component = {Controller} />
       </Stack.Navigator>
     </NavigationContainer>
 
