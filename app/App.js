a7784f6b63a4442d0b6f68c308de776b61613961
Jonathan Garduno <jonathan.garduno@student.csulb.edu>
2023-05-09 00:25:58 -0700

    update

--- app/App.js
+++ app/App.js
@@ -6,7 +6,7 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   SafeAreaView,
   Text,
@@ -19,50 +19,116 @@ import {TourGuide} from './screens'
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native'; //contains navigator and screen
 import { Screen } from 'react-native-screens';
+import {firebase} from './screens/config';
 
 //krimika's controller thing:
 import {Controller2} from './screens';
 
+import Login from './screens/Login';
+import Registration from './screens/Registration';
+import Dashboard from './screens/Dashboard';
+import Header from './screens/Header';
+
 const Stack = createNativeStackNavigator()
-/*returning the quiz layout and functionality*/
-const App = () => {
-  
-  return (
-    <NavigationContainer>
 
+function App(){
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+
+  function onAuthStateChanged(user){
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = firebase.auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, [] );
+
+  if (initializing) return null;
+  if (!user){
+    return (
       <Stack.Navigator>
-        <Stack.Screen
-          name = "HomeScreen"
-          component={HomeScreen}
-        
-          />
-        <Stack.Screen
-          name = "TourGuide"
-          component = {TourGuide}
-          />
-          
-        <Stack.Screen
-          name = "DestinationQueue"
-          component = {DestinationQueue}
-
-        />
-        <Stack.Screen
-          name = "QuizApp"
-          component = {Quiz}
-        />
-        
         <Stack.Screen 
-          name = "Controller2"
-          component = {Controller2} />
-      </Stack.Navigator>
-    </NavigationContainer>
+        name="Login" 
+        component={Login}
+        options={{
+          headerTitle: () => <Header name="Login" />,
+          headerStyle: {
+            height: 150,
+            borderBottomLeftRadius:50,
+            borderBottomRightRadius:50,
+            backgroundColor:'#00e4d0',
+            shadowColor: '#000',
+            elevation:25
+          }
+        }} 
+        />
 
-    
-    // <Quiz/>
-    // <DestinationQueue/>
+        
+
+      <Stack.Screen
+         name= "Registration"
+         component={Registration}
+         options={{
+          headerTitle: () => <Header name= "Registration"/>,
+          headerStyle:{
+            height:150,
+            borderBottomLeftRadius:50,
+            borderBottomRightRadius:50,
+            backgroundColor: '#00e4d0',
+            shadowColor: '#000',
+            elevation: 25
+          }
+
+         }}
+        />
+      </Stack.Navigator>
+    );
+  }
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name = "HomeScreen" component={HomeScreen}/>
+        <Stack.Screen name = "TourGuide" component = {TourGuide}/>
+        <Stack.Screen name = "DestinationQueue" component = {DestinationQueue}/>
+        <Stack.Screen name = "QuizApp" component = {Quiz}/>
+        <Stack.Screen name = "Controller2" component = {Controller2} />
+    </Stack.Navigator>
   );
 }
 
+export default () => {
+  return (
+    <NavigationContainer>
+      <App/>
+    </NavigationContainer>
+  )
+}
+// /*returning the quiz layout and functionality*/
+// const App = () => {
+  
+//   return (
+//     <NavigationContainer>
+
+//       <Stack.Navigator>
+//         <Stack.Screen name="Login" component={Login} />
+//         <Stack.Screen name = "HomeScreen" component={HomeScreen}/>
+//         <Stack.Screen name = "TourGuide" component = {TourGuide}/>
+//         <Stack.Screen name = "DestinationQueue" component = {DestinationQueue}/>
+//         <Stack.Screen name = "QuizApp" component = {Quiz}/>
+//         <Stack.Screen name = "Controller2" component = {Controller2} />
+        
+//         <Stack.Screen name="Registration" component={Registration} />
+//         <Stack.Screen name="Dashboard" component={Dashboard} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+
+    
+//     // <Quiz/>
+//     // <DestinationQueue/>
+//   );
+// }
 
 
-export default App;
+
+// export default App;
